620f61b33bd5518f7b2f2391cc6597cbee3a174e
shreyarg6 <147192995+shreyarg6@users.noreply.github.com>
2023-11-16 18:30:04 -0500

    Update QuestionSolutions.sql

--- QuestionSolutions.sql
+++ QuestionSolutions.sql
@@ -1,11 +1,29 @@
 CREATE OR REPLACE PACKAGE youtubeData AS
 
-  FUNCTION (keywordV IN VARCHAR2) RETURN number;
+  FUNCTION HighestViewCount RETURN videoKeyword;
 
 END;
-# Question 1
 
+# Question 1 --------------------------------------
+create or replace function HighestViewCount
+    return varchar2
+as
+    highestVideo varchar2(35);
+begin
+    select userhistory.videoKeyword
+        into highestVideo
+    from UserHistory 
+    inner join on UserInteractions.UserID=UserHistory.UserID
+    order by UserInteractions.numberOfViews
+    fetch first row only;
+    
+    return highestVideo;
+end NUMBER_OF_EMPLOYEES;
+
+# test function
+select HighestViewCount()
+from dual;
   
-# Question 2
+# Question 2 --------------------------------------
 
-# Question 3
+# Question 3 --------------------------------------
