ed02b0e3492b321c3fc61caec3763cbcfc2d333f
shreyarg6 <147192995+shreyarg6@users.noreply.github.com>
2023-11-16 20:05:00 -0500

    Update QuestionSolutions.sql

--- QuestionSolutions.sql
+++ QuestionSolutions.sql
@@ -22,14 +22,37 @@ end NUMBER_OF_EMPLOYEES;
 
 # test function
 DECLARE
-  highestViewedVideoType;
+  highestViewedVideoType varchar2(35);
 BEGIN
   highestViewedVideoType := HighestViewCount();
   DBMS_OUTPUT.PUT_LINE('The videos with the highest views are ' || highestViewedVideoType);
 END;
   
 # Question 3: Which type of video has the highest engagement (comments + likes) and how does this correlate with the age of the viewer?
+create or replace function Engagement
+    (VideoType Out varchar2 (35),
+    VideoAge Out number)
+as
+    VideoType varchar2(35);
+    VideoAge number;
+begin
+    select userhistory.videoKeyword into VideoType, userinfo.age into VideoAge
+    from UserHistory 
+    inner join on UserInteractions.UserID=UserHistory.UserID
+    inner join on UserHistory.UserId = UserInfo.UserID
+    where max(UserInteractions.numberOfLikes + UserInteractions.numberOfComments)
+    
+    return VideoType, VideoAge;
+end NUMBER_OF_EMPLOYEES;
 
+# test function
+DECLARE
+  VideoType varchar2(35);
+  VideoAge number;
+BEGIN
+  highestViewedVideoType := HighestViewCount();
+  DBMS_OUTPUT.PUT_LINE('The videos with the highest views are ' || highestViewedVideoType);
+END;
   
 # Question 4 
 
