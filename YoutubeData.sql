8a773754347d4f36fa1beafdfcc357701af0f1bc
rishikathorat <32944692+rishikathorat@users.noreply.github.com>
2023-11-17 09:42:30 -0500

    Created UserInfo table and inserted data for 4 users with unique IDs and specific ages.

--- YoutubeData.sql
+++ YoutubeData.sql
@@ -1,1 +1,19 @@
+CREATE TABLE UserInfo (
+    UserID NUMBER PRIMARY KEY,
+    Age NUMBER,
+);
+
+INSERT INTO UserInfo (UserID, Age)
+VALUES (1, 25);
+
+INSERT INTO UserInfo (UserID, Age)
+VALUES (2, 30);
+
+INSERT INTO UserInfo (UserID, Age)
+VALUES (3, 20);
+
+INSERT INTO UserInfo (UserID, Age)
+VALUES (4, 50);
+
+COMMIT;
 
