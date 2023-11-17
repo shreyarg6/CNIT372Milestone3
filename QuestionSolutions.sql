0ae61821c50fad8ba635dab0e0c0a3223986b728
rishikathorat <32944692+rishikathorat@users.noreply.github.com>
2023-11-17 16:31:08 -0500

    Update QuestionSolutions.sql

--- QuestionSolutions.sql
+++ QuestionSolutions.sql
@@ -57,33 +57,64 @@ END;
   
 # Question 2
 
-SELECT 
-    UserInfo.AgeRange,
-    UserHistory.VideoKeyword,
-    MAX(UserInteractions.NumberOfLikes) AS MaxLikes
-FROM UserInfo
-JOIN UserHistory ON UserInfo.UserID = UserHistory.UserID
-JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
-GROUP BY UserInfo.AgeRange, UserHistory.VideoKeyword;
+CREATE OR REPLACE PROCEDURE GetPopularVideosByAge AS
+BEGIN
+    FOR AgeRangeInfo IN (SELECT DISTINCT AgeRange FROM UserInfo)
+    LOOP
+        FOR VideoInfo IN (
+            SELECT
+                UserInfo.AgeRange,
+                UserHistory.VideoKeyword,
+                MAX(UserInteractions.NumberOfLikes) AS MaxLikes
+            FROM UserInfo
+            JOIN UserHistory ON UserInfo.UserID = UserHistory.UserID
+            JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
+            WHERE UserInfo.AgeRange = AgeRangeInfo.AgeRange
+            GROUP BY UserInfo.AgeRange, UserHistory.VideoKeyword
+        )
+        LOOP
+            DBMS_OUTPUT.PUT_LINE('Age Range: ' || AgeRangeInfo.AgeRange || ', Video Keyword: ' || VideoInfo.VideoKeyword || ', Max Likes: ' || VideoInfo.MaxLikes);
+        END LOOP;
+    END LOOP;
+END GetPopularVideosByAge;
+/
 
 # Question 7 
 
-SELECT
-    UserHistory.VideoKeyword,
-    SUM(UserInteractions.NumberOfLikes) AS TotalLikes,
-    SUM(UserInteractions.NumberOfComments) AS TotalComments,
-    SUM(UserInteractions.NumberOfSubscriptions) AS TotalSubscriptions
-FROM UserHistory
-JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
-GROUP BY UserHistory.VideoKeyword
-ORDER BY TotalLikes DESC, TotalComments DESC, TotalSubscriptions DESC;
+CREATE OR REPLACE PROCEDURE GetMostInteractiveVideos AS
+BEGIN
+    FOR VideoInfo IN (
+        SELECT
+            UserHistory.VideoKeyword,
+            SUM(UserInteractions.NumberOfLikes) AS TotalLikes,
+            SUM(UserInteractions.NumberOfComments) AS TotalComments,
+            SUM(UserInteractions.NumberOfSubscriptions) AS TotalSubscriptions
+        FROM UserHistory
+        JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
+        GROUP BY UserHistory.VideoKeyword
+        ORDER BY TotalLikes DESC, TotalComments DESC, TotalSubscriptions DESC
+    )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Video Keyword: ' || VideoInfo.VideoKeyword || ', Total Likes: ' || VideoInfo.TotalLikes || ', Total Comments: ' || VideoInfo.TotalComments || ', Total Subscriptions: ' || VideoInfo.TotalSubscriptions);
+    END LOOP;
+END GetMostInteractiveVideos;
+/
 
 # Question 8 
 
-SELECT
-    UserHistory.VideoKeyword,
-    AVG(UserInteractions.NumberOfComments) AS AvgComments
-FROM UserHistory
-JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
-GROUP BY UserHistory.VideoKeyword
-ORDER BY AvgComments DESC;
+CREATE OR REPLACE PROCEDURE GetAverageCommentsPerVideo AS
+BEGIN
+    FOR VideoInfo IN (
+        SELECT
+            UserHistory.VideoKeyword,
+            AVG(UserInteractions.NumberOfComments) AS AvgComments
+        FROM UserHistory
+        JOIN UserInteractions ON UserInteractions.UserID = UserHistory.UserID
+        GROUP BY UserHistory.VideoKeyword
+        ORDER BY AvgComments DESC
+    )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Video Keyword: ' || VideoInfo.VideoKeyword || ', Average Comments: ' || VideoInfo.AvgComments);
+    END LOOP;
+END GetAverageCommentsPerVideo;
+/
